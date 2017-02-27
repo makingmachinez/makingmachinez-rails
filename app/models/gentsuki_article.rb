ffb83f0dfac744fb08fa23b730af7a736748483a
funnythingz <funnythingz69+github@gmail.com>
2017-02-28 01:25:32 +0900

    next and prev of GentsukiArticle

--- app/models/gentsuki_article.rb
+++ app/models/gentsuki_article.rb
@@ -1,2 +1,10 @@
 class GentsukiArticle < ApplicationRecord
+
+  def next
+    self.class.unscoped.where("id > ?", id).limit(1).first
+  end
+
+  def prev
+    self.class.unscoped.where("id < ?", id).limit(1).first
+  end
 end
